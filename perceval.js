0af504167bb5980ce58344debb0b8a5ad6abe3b9
Alex Soyer <dinath.contact@gmail.com>
2017-01-23 09:44:25 +0100

    starting from api ok, only one question, bad display

--- perceval.js
+++ perceval.js
@@ -1,2 +1,32 @@
 var QuizBot = require('slackquizbot');
-var myQuizBot = new QuizBot("xoxb-111352045732-H0V2aftGiXcl5Ac7M3q0dsG5", "fr");
+var express = require('express');
+
+var myQuizBot = new QuizBot("xoxb-111352045732-3f0uGWmsMZ7p3cirmJ1JT0H1", "fr");
+
+// console.log('QuizBot ' + myQuizBot);
+
+// myQuizBot.startQuiz();
+
+var app = express();
+
+var server = app.listen(8081, function () {
+    console.log("Example app listening");
+})
+
+app.post('/api/github', function (req, res) {
+
+
+    res.send('hello world');
+});
+
+var slackChanel = "C3DGRQNSF";
+var quizId= "myquiz";
+app.get('/api/quiz-start', function (req, res) {
+    try {
+        var c = myQuizBot.getContext();
+        console.log(c);
+        myQuizBot.startQuiz(quizId, slackChanel, quizId);
+    } catch (err) {
+        console.log(err);
+    }
+});
